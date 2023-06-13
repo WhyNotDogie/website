1a15e9e367a3a33d98dc1ed89a8beeaa1263b469
Kees Johnson <keesjejohnson@gmail.com>
2023-06-13 18:16:14 +0200

    fix

--- src/lib/main.svelte
+++
@@ -1,7 +0,0 @@
-<header><a href="./projects">My Projects</a></header>
-
-<style>
-	:global(:root) {
-		font-family: 'Comfortaa', cursive;
-	}
-</style>
--- src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import App from '../lib/main.svelte';
-</script>
-
-<App />
